111ebf158185ef80e5120f09e7b3d4550e00456f
Osama Qutait <102440794+OsamaQutait@users.noreply.github.com>
2022-08-15 11:14:10 +0300

    added implicitly Wait

--- src/main/java/w3schoolSearch.java
+++ src/main/java/w3schoolSearch.java
@@ -1,6 +1,8 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
+import java.time.Duration;
+
 public class w3schoolSearch {
     WebDriver driver;
     public void launchBrowser(){
@@ -9,11 +11,12 @@ public class w3schoolSearch {
         driver.get("https://www.w3schools.com/#gsc.tab=0");
     }
     public void searchProduct() throws InterruptedException {
-        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(By.id("search2")).sendKeys("HTML");
         driver.findElement(By.id("learntocode_searchbtn")).click();
-        Thread.sleep(2000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.findElement(By.linkText("HTML Tutorial")).click();
+
     }
     public static void main(String[] args) throws InterruptedException {
         w3schoolSearch search_about_selenium = new w3schoolSearch();
